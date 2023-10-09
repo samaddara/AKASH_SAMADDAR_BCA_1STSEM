556b7950cb1d62354194cf47ab7fd179f0671e52
samaddara <144552807+samaddara@users.noreply.github.com>
2023-10-09 11:17:39 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/age.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int age;
+	printf("enter the age");
+	scanf("%d",&age);
+	if(age>=18  && age<=100){
+		printf("you are eligible to vote");
+	}
+	else("you are underage");
+	}
+
